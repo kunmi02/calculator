99fb9cac0558ab1127aecf7f5a9a7494fbdd7650
kunmi02 <mrkhunmie@gmail.com>
2021-08-04 14:14:33 +0100

    formatted the math magician page and added tests to it

--- src/components/__tests__/button.test.js
+++ src/components/__tests__/button.test.js
@@ -3,9 +3,16 @@ import renderer from 'react-test-renderer';
 import Button from '../button';
 
 it('renders correctly', () => {
-  const isWide = true;
+  const isWide = '25%';
+  const styles = {
+    width: '25%',
+    background: 'orange',
+    height: '100%',
+    font: "3rem 'Syne Mono'",
+    textAlign: 'center',
+  };
   const tree = renderer
-    .create(<Button name="0" key="0" clickHandler={() => {}} wide={isWide} />)
+    .create(<Button name="0" key="0" style={styles}  clickHandler={() => {}}  wide={isWide} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,17 @@
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { name, clickHandler } = props;
+  const {
+    name, clickHandler, color, wide,
+  } = props;
+
+  const styles = {
+    width: wide,
+    background: color,
+    height: '100%',
+    font: "3rem 'Syne Mono'",
+    textAlign: 'center',
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -9,13 +19,20 @@ const Button = (props) => {
   };
 
   return (
-    <button type="button" onClick={handleClick}>{ name }</button>
+    <button type="button" style={styles} onClick={handleClick}>{ name }</button>
   );
+};
+
+Button.defaultProps = {
+  color: 'orange',
+  wide: '25%',
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  wide: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default Button;
